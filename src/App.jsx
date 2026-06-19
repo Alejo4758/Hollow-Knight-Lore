40e2c0303008c2dcac95741c1ad1f1846488505b
Alejo <LopezAlejo0124@gmail.com>
2026-06-19 19:25:59 -0300

    Integración en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import HeroSection from "./components/HeroSection/HeroSection";
 import CharacterSection from "./components/CharacterSection/CharacterSection";
+import { getCharacters } from "./services/CharacterService";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function App () {
+    const [characters, setCharacters] = useState ([]);
+
+    useEffect (() => {
+        const fetchCharacters = async () => {
+            const { data, error } = await getCharacters ();
+            if (!error) setCharacters (data);
+        }
+        fetchCharacters ();
+    }, []);
+
+    console.log (characters);
+    
     return (
         <>
             <HeroSection/>
-            <CharacterSection/>
+            <CharacterSection characters={characters}/>
         </>
     )
 }
