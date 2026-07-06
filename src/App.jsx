337c507919a2215147b913b728ff7930f79a4857
Alejo <LopezAlejo0124@gmail.com>
2026-07-06 15:29:06 -0300

    Integración del componente en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function App () {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
             </Routes>
