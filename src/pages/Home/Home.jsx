72d436b37f2559f131a772501de995bae318d294
Alejo <LopezAlejo0124@gmail.com>
2026-07-08 13:49:19 -0300

    Correcciones menores para la consistencia visual

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,13 @@ import hkVideo from "../../assets/home.mp4";
 
 export default function Home () {
     return (
-        <main className="relative w-dvw h-dvh overflow-hidden bg-bg-base text-text-primary">
+        <main className="relative w-full h-dvh overflow-hidden bg-bg-base text-text-primary">
 
-            <video className="absolute top-0 left-0 w-full h-full object-cover z-0 opacity-70" autoPlay loop muted playsInline preload="auto">
+            <video className="fixed top-0 left-0 w-full h-full object-cover z-0 opacity-70" autoPlay loop muted playsInline preload="auto">
                 <source src={hkVideo} type="video/mp4" />
             </video>
 
-            <div className="absolute inset-0 bg-linear-to-r from-bg-base via-bg-base/40 to-transparent z-10 pointer-events-none"/>
+            <div className="absolute inset-0 bg-bg-base/60 z-10 pointer-events-none"/>
 
             <section className="relative z-20 flex flex-col justify-center h-full w-full px-6 md:px-24 max-w-md md:max-w-2xl space-y-6 md:space-y-8">
                 <div className="flex flex-col items-start">
@@ -28,7 +28,7 @@ export default function Home () {
                     <div className="h-px w-16 md:w-24 bg-linear-to-l from-transparent to-accent" />
                 </div>
 
-                <Link to="/contents" className="group relative inline-flex items-center justify-center px-8 py-3 mt-4 font-cinzel text-sm md:text-base tracking-[0.2em] text-text-primary uppercase transition-all duration-500 border border-accent/30 bg-bg-elevated/20 backdrop-blur-sm hover:bg-accent/10 hover:border-accent/80 hover:shadow-accent hover:-translate-y-1 overflow-hidden">
+                <Link to="/explore" className="group relative inline-flex items-center justify-center px-8 py-3 mt-4 font-cinzel text-sm md:text-base tracking-[0.2em] text-text-primary uppercase transition-all duration-500 border border-accent/30 bg-bg-elevated/20 backdrop-blur-sm hover:bg-accent/10 hover:border-accent/80 hover:shadow-accent hover:-translate-y-1 overflow-hidden">
                     <span className="relative z-10 transition-colors duration-300 group-hover:text-white drop-shadow-md">
                         Comienza a explorar...
                     </span>
